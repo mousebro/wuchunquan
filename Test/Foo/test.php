5ff573141018aa4990d37a2485ac9907b030576a
chenguangpeng <1031289030@qq.com>
2016-02-22 16:25:22 +0800

    测试程序

---
+++ Test/Foo/test.php
@@ -0,0 +1,24 @@
+<?php
+use Model\Demo\Foo;
+use Model\Demo\Bar;
+include 'init.php';
+//\Model\Member\Member::say();
+
+//$name, $tablePrefix,
+//print_r(C('db')['localhost']);
+$connection = C('db')['localhost'];
+$foo = new Foo('member','pft_',$connection);
+$bar = new Bar('jq_ticket','uu_',$connection);
+var_dump($foo->show_ticket(800,  $bar));
+print_r($bar->call_procudure());
+//$member->members();
+//print_r($member->findMemberById(94));
+
+
+//$res = $member->register(['dname'=>'事务测试','cname'=>'thinkphp', 'account'=>'dev_cgp_'.mt_rand(100000,999999)],[]);
+//var_dump($res);
+//print_r($member->updateMember(94));
+//print_r($member->findMemberById(94));
+//\Model\Member\Foo::say();
+/*print_r(C('db'));*/
+//print_r(C('db'));
