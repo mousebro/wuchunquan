a44e784f352210a80e78b01ce4265f397a81a090
chenguangpeng <1031289030@qq.com>
2016-02-23 09:11:13 +0800

    add config files

---
+++ Conf/config_develop.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Guangpeng Chen
+ * Date: 2/19-019
+ * Time: 11:57
+ */
+return array(
+    'db'=> array(
+        'localhost' => array(
+            'db_type'=>'mysql',
+            'db_host' => '192.168.20.138',
+            'db_user' => 'develop',
+            'db_pwd' => 'develop%',
+            'db_port' => 3307,
+            'db_name' => 'myuu',
+        ),
+        'remote_1' => array(
+            'db_type'=>'mysql',
+            'db_host' => '192.168.20.138',
+            'db_user' => 'adminSS',
+            'db_pwd' => '5f40019b80@pft',
+            'db_port' => 3307,
+            'db_name' => 'myuu_ss',
+        ),
+        //10.160.4.140
+        'remote_2' => array(
+            'db_type'=>'mysql',
+            'db_host'=>'10.117.7.197',
+            'db_user' => 'pft_user_140',
+            'db_pwd' => '5f40019b80@pft',
+            'db_port' => 3307,
+            'db_name' => 'myuu',
+        ),
+    ),
+
+
+
+);
