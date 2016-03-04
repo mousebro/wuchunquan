aeb72dbcbf2ad6733065f51a52f4a60f0e5cb344
chenguangpeng <1031289030@qq.com>
2016-03-04 11:38:19 +0800

    init文件修改

--- init.php
+++ init.php
@@ -5,14 +5,15 @@
  * Date: 2/18-018
  * Time: 17:13
  */
-include 'autoload.php';
-//include_once dirname(__FILE__).'/../wx/wechat/autoloader.php';
-include 'Common/functions.php';
-include 'Conf/pft.conf.php';
+include_once 'autoload.php';
+include_once 'Common/functions.php';
+include_once 'Conf/pft.conf.php';
 C(include  'Conf/config_'.strtolower(ENV).'.php');
+spl_autoload_register("\\AutoLoading\\loading::autoload");
+
 //spl_autoload_register("\\AutoLoading\\loading::autoload");
 //spl_autoload_register("\\AutoLoading\\loading::load_lane_wechat");
 //\LaneWeChat\Autoloader::register();
-AutoLoading\loading::register();
+//AutoLoading\loading::register();
 # include '/var/www/html/new/d/common/func.inc.php';
 
