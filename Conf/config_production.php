933ce806cba196dbd02af0524fdc855edee32a83
chenguangpeng <1031289030@qq.com>
2016-03-09 09:08:48 +0800

    修改数据库连接对应app服务器

--- Conf/config_production.php
+++ Conf/config_production.php
@@ -10,7 +10,6 @@ return array(
         'localhost' => array(
             'db_type'=>'mysql',
             'db_host' => '10.160.4.140',
-            //'db_host' => '127.0.0.1',
             'db_user' => 'admin',
             'db_pwd' => 'adm*753951',
             'db_port' => 3306,
@@ -19,7 +18,6 @@ return array(
         'localhost_wsdl' => array(
             'db_type'=>'mysql',
             'db_host' => '10.160.4.140',
-            //'db_host' => '127.0.0.1',
             'db_user' => 'wsdl',
             'db_pwd' => 'ws*753951',
             'db_port' => 3306,
