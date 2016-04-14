ea1eee3cc49967dc36c13985084c07e2468b0a24
dewei <zhang_dewei@qq.com>
2016-04-14 19:23:52 +0800

    自动提现功能初始化

---
+++ Api/withdraw.class.php
@@ -0,0 +1,42 @@
+<?php
+namespace Api;
+
+if(!defined('PFT_API')) {exit('Access Deny');}
+
+/**
+ * 现金提现提供给民生专线的接口
+ *
+ * @author dwer
+ * @date 2016-04-13
+ */
+
+use Library\Controller;
+
+class withdraw extends Controller{
+    /**
+     * 获取需要进行提现的列表
+     * @author dwer
+     * @date   2016-04-14
+     *
+     * @return
+     */
+    public function getList() {
+        $limit = intval($this->getParam('limit'));
+        $limit = $limit < 1 ? 100 : ($limit > 100 ? 100 : $limit);
+
+        $withdrawModel = $this->model('');
+        $list = 
+    }
+
+    /**
+     * 接受民生订单的反馈信息
+     * @author dwer
+     * @date   2016-04-14
+     *
+     * @return
+     */
+    public function feedback() {
+
+    }
+
+}
---
+++ Library/ApiDispatch.class.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * API接口进行统一的路由和交易
+ * @author zdw
+ * @date 2016-04-14
+ * 
+ */
+namespace Library;
+
+class ApiDispatch{
+
+    public static function run(){
+        static $_codeAr = array();
+
+        $isAuth = self::_auth();
+        if(!$isAuth) {
+            self::error(401, '未授权');
+        }
+
+        $_routeAr = self::_getRequestRoute();
+        $key      = $_routeAr['c'] . '_' . $_routeAr['a'];
+// var_dump($_routeAr['c']);die;
+        if (class_exists($_routeAr['c'])){
+            $_object = new $_routeAr['c']();
+        } else {
+            self::error(400, 'Controller Not Exist');
+        }
+
+        if (method_exists($_routeAr['c'], $_routeAr['a'])) {
+            $_codeAr[$key] = $_object->$_routeAr['a']();
+        } else {
+            self::error(400, 'Action Not Exist');
+        }
+    }
+
+    /**
+     * 错误处理
+     *
+     * @access private
+     * @return void
+     */
+    public static function error($code, $message='') {
+        $data = array(
+            'code'    => $code,
+            'message' => $message
+        );
+
+        echo json_encode($data);
+        exit();
+    }
+
+    /**
+     * api接口授权验证
+     * @author dwer
+     * @date   2016-04-14
+     *
+     * @return
+     */
+    private static function _auth() {
+        return true;
+
+    }
+
+    private static function _getRequestRoute(){
+        $controller = I('get.c');
+        if (strpos($controller, '_')!==false) {
+            list($namespace, $controller) = explode('_', $controller);
+            $controller = $namespace . '/' . $controller;
+        }
+        $action     = I('get.a');
+        $controller = 'Api\\' . str_replace('/', '\\', $controller);
+        return [
+            'c'=>$controller,
+            'a'=>$action
+        ];
+    }
+}
