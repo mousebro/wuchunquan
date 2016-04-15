3de56237dd6c08b57b0df0ad216cb240c686373d
fangli <1330456057>
2016-04-15 09:50:22 +0800

    限制计调下单只有云顶景区和部分测试账号可见

--- Controller/DispatchOrder.class.php
+++ Controller/DispatchOrder.class.php
@@ -4,41 +4,61 @@
  * Time: 14:45 2016/3/8
  */
 namespace Controller;
+
 use Library\Controller;
+use Model\Member\Member;
 use Model\Member\MemberRelationship;
 
-class DispatchOrder extends Controller{
+class DispatchOrder extends Controller
+{
     private $memberId;
+
     public function __construct()
     {
-        $this->memberId=$this->isLogin('ajax');
+        $this->memberId = $this->isLogin('ajax');
+        $this->checkAuth($this->memberId);
     }
 
-    public function getDistributor(){
+    /**
+     * 获取分销商列表
+     */
+    public function getDistributor()
+    {
         $search = strval(I('param.search'));
-        $page = intval(I('param.page'));
-        $limit = intval(I('param.limit'));
-        if(!$page) $page = 1;
-        $limit = $page ? ($limit ? $limit : 20) :9999;
-        $relationModel = new MemberRelationship($this->memberId);
-        $distributor = $relationModel -> getDistributor($search,$page,$limit);
-        $total = $relationModel -> getDistributor($search,$page,$limit,true);
-        if(is_array($distributor) && count($distributor)>0){
-            $data = array(
-                "page" => $page,
-                "limit" => $limit,
-                "total" => $total,
-                "list" => $distributor
-            );
-
-        }else{
-            $data = array(
-                "page" => $page,
-                "limit" => $limit,
-                "total" => 0,
-                "list" => []
-            );
+        $page   = intval(I('param.page'));
+        $limit  = intval(I('param.limit'));
+        if ( ! $page) {
+            $page = 1;
         }
-        $this->apiReturn(200,$data,'操作成功');
+        $limit         = $page ? ($limit ? $limit : 20) : 9999;
+        $relationModel = new MemberRelationship($this->memberId);
+        $distributor   = $relationModel->getDistributor($search, $page, $limit);
+        $total         = $relationModel->getDistributor($search, $page, $limit, true);
+        if (is_array($distributor) && count($distributor) > 0) {
+            $data = array("page" => $page, "limit" => $limit, "total" => $total, "list" => $distributor);
+        } else {
+            $data = array("page" => $page, "limit" => $limit, "total" => 0, "list" => []);
+        }
+        $this->apiReturn(200, $data, '操作成功');
+    }
+
+    /**
+     * 检查是否有计调下单权限
+     * @param $memberId
+     */
+    private function checkAuth($memberId)
+    {
+        if (in_array($memberId, [3385, 6197,57675])) {
+            return;
+        }
+        $memberModel = new Member();
+        $member_info = $memberModel->getMemberInfo($memberId);
+        if ($member_info) {
+            $member_group = $member_info['group_id'];
+            if (in_array($member_group, [2,4])) {
+                return;
+            }
+        }
+        $this->apiReturn(204,[],'当前账号无计调下单权限');
     }
 }
