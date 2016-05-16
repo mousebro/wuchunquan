9b8cf140947438d9853d16b9bd7dba24f377b120
Guangpeng Chen <1031289030@qq.com>
2016-05-17 00:26:37 +0800

    订单统计：统计原始供应商每日的产品销量

---
+++ Controller/cli/OrderReport.class.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cgp
+ * Date: 16/5/16
+ * Time: 23:18
+ */
+
+namespace Controller\cli;
+if (PHP_SAPI !='cli') exit('error');
+defined('PFT_INIT') or exit('Access Denied');
+class OrderReport
+{
+    public function OrderSummaryByLid()
+    {
+        $date = date('Ymd', strtotime('-1 days'));
+        $model = new \Model\Report\OrderReport();
+        $startTime = strtotime('2016-01-01');
+        $endTime   = '2016-05-17';
+        $diff      = (strtotime($endTime) - $startTime) / 86400;
+        for ($i=0; $i< $diff; $i++) {
+            $date = date('Ymd', strtotime("+ $i days ", $startTime));
+            echo $date,"\n";
+            $model->OrderSummaryByLid($date);
+        }
+    }
+}
