c77b9c801dab62e3e0d2c503a75ef98a42aea699
dewei <zhang_dewei@qq.com>
2016-05-17 17:53:23 +0800

    演出类产品分销库存调整为针对一级分销商

--- Model/Product/YXStorage.class.php
+++ Model/Product/YXStorage.class.php
@@ -146,6 +146,7 @@ class YXStorage extends Model{
         $reserveNum = $info['reserve_num'];
 
         $resStorage = 0;
+
         if($sellerStorage == -1) {
             //使用未分配的库存
             $leftStorage = $this->_getLeftStorage($roundId, $areaId, $reserveNum);
@@ -857,14 +858,15 @@ class YXStorage extends Model{
             'status'   => array('in', '2, 3')
         );
 
+        //获取的是一级分销商的数据
         if($resellerId) {
-            $where['opid'] = $resellerId;
+            $where['top_reseller_id'] = $resellerId;
         }
 
         $sales = $this->table($this->_dynTable)->where($where)->count();
         $sales = $sales === false ? 0 : intval($sales);
 
-        return $sales;
+        return $sales; 
     }
 
     /**
