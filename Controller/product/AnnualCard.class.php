979bd0e1f44a93b0ecf360d7982435fe8c9d3979
wengbin <379591850@qq.com>
2016-05-31 17:53:59 +0800

    update

--- Controller/product/AnnualCard.class.php
+++ Controller/product/AnnualCard.class.php
@@ -7,13 +7,36 @@ use Model\Product\AnnualCard as CardModel;
 
 class AnnualCard extends Controller {
 
+    public function __construct() {
+
+        if (!isset($_SESSION['memberID'])) {
+            $this->apiReturn(403, [], '请先登录');
+        }
+
+    }
+
     /**
      * 获取所属供应商的年卡产品列表
      * @return [type] [description]
      */
     public function getAnnualCardProducts() {
 
-        $this->_initializeCardModel()->getAnnualCardProducts();
+        $this->_initializeCardModel()->getAnnualCardProducts($_SESSION['sid']);
+    }
+
+    /**
+     * 获取指定产品的关联年卡
+     * @return [type] [description]
+     */
+    public function getAnnualCards() {
+        $pid = I('pid', '', 'intval');
+
+        if (intval($pid) < 1) {
+            $this->apiReturn(400, [], '参数错误');
+        }
+
+        $this->_initializeCardModel()->getAnnualCards($_SESSION['sid'], $pid);
+
     }
 
 
@@ -22,8 +45,15 @@ class AnnualCard extends Controller {
      * @return [type] [description]
      */
     public function createAnnualCard() {
+        $num = I('num', 1, 'intval');   //创建数量
 
-        $CardModel = $this->_initializeCardModel()->createAnnualCard();
+        $pid = I('pid', '', 'intval');
+
+        if (intval($num) < 1 || $num > 100 || intval($pid) < 1) {
+            $this->apiReturn(400, [], '参数错误');
+        }
+
+        $CardModel = $this->_initializeCardModel()->createAnnualCard($num, $sid, $pid);
 
     }
 
@@ -34,9 +64,40 @@ class AnnualCard extends Controller {
      */
     public function deleteAnnualCard() {
 
-        $this->_initializeCardModel()->deleteAnnualCard();
+        if (isset($_GET['virtual_no'])) {
+            $where = [
+                'sid'        => $_SESSION['sid'],
+                'virtual_no' => I('virtual_no'),
+                'status'     => 3
+            ];
+        } else {
+            $where = [
+                'sid'        => $_SESSION['sid'],
+                'status'     => 3
+            ];
+        }
+
+        $this->_initializeCardModel()->deleteAnnualCard($where);
 
     }
+
+    /**
+     * 绑定物理卡（完善年卡信息）
+     * @return [type] [description]
+     */
+    public function bindAnnualCard() {
+        $card_no    = I('card_no');
+        $physics_no = I('physics_no');
+        $virtual_no = I('virtual_no');
+
+        if (!$card_no || !$physics_no || !$virtual_no) {
+            $this->apiReturn(400, [], '参数错误');
+        }
+
+        $this->bindAnnualCard($_SESSION['sid'], $virtual_no, $card_no, $physics_no);
+    }
+
+
 
 
 
--- Model/Product/AnnualCard.class.php
+++ Model/Product/AnnualCard.class.php
@@ -8,6 +8,35 @@ class AnnualCard extends Model {
 
     const ANNUAL_CARD_TABLE = 'pft_annual_card';    //卡片信息表
 
+    const PRODUCT_TABLE     = 'uu_products';        //产品信息表
+
+    /**
+     * 根据字段获取年卡信息
+     * @param  [type] $identify [description]
+     * @param  string $field    [description]
+     * @return [type]           [description]
+     */
+    public function getAnnualCard($identify, $field = 'id') {
+
+        return $this->table(self::ANNUAL_CARD_TABLE)->where([$field => $identify])->find();
+
+    }
+
+     /**
+     * 获取指定产品的关联年卡
+     * @return [type] [description]
+     */
+    public function getAnnualCards($sid, $pid, $options = []) {
+
+        $where = [
+            'sid' => $sid,
+            'pid' => $pid
+        ];
+
+        $this->table(self::ANNUAL_CARD_TABLE)->where($where)->select($options);
+
+    }
+
     /**
      * 获取指定供应商的年卡产品列表
      * @param  [type] $sid 供应商id
@@ -15,23 +44,121 @@ class AnnualCard extends Model {
      */
     public function getAnnualCardProducts($sid) {
 
+        return $this->table(self::PRODUCT_TABLE)->where(['apply_did' => $sid, 'p_type' => 'I'])->select();
+
     }
 
     /**
-     * 录入年卡
+     * 生成年卡
      * @return [type] [description]
      */
-    public function createAnnualCard() {
+    public function createAnnualCard($num, $sid, $pid) {
+        $insert_data = [];
 
+        while (1) {
+            $virtual_no = $this->_createVirtualNo();
+
+            if (!$this->getAnnualCard($virtual_no, 'virtual_no')) {
+                $insert_data[] = ['sid' => $sid, 'pid' => $pid, 'virtual_no' => $virtual_no];
+            }
+
+            if (count($virtual_arr) == $num) break;
+        }
+
+        if ($this->table(self::ANNUAL_CARD_TABLE)->addAll($insert_data)) {
+            return false;
+        }
+
+        return $insert_data;
+    }
+
+    /**
+     * 生成虚拟卡号
+     * @return [type] [description]
+     */
+    private function _createVirtualNo() {
+        $string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $number = '0123456789';
+        $mix    = $string . $number;
+
+        $virtual_no     = '';
+        $head           = str_shuffle($string)[0];
+        $second_part    = substr(str_shuffle($string), 0, 3);
+        $third_part     = substr(str_shuffle($number), 0, 3);
+        $virtual_no    .= $head . $second_part . $third_part;
+        $tail           = array_sum(str_split($virtual_no));
+        $virtual_no    .= $virtual_no . $tail;
+
+        return $virtual_no;
     }
 
     /**
      * 删除年卡
      * @return [type] [description]
      */
-    public function deleteAnnualCard() {
+    public function deleteAnnualCard($where) {
+        //TODO:log it
+        return $this->table(self::ANNUAL_CARD_TABLE)->where($where)->delete();
 
     }
 
+    /**
+     * 绑定物理卡
+     * @param  [type] $sid        [description]
+     * @param  [type] $virtual_no [description]
+     * @param  [type] $card_no    [description]
+     * @param  [type] $physics_no [description]
+     * @return [type]             [description]
+     */
+    public function bindAnnualCard($sid, $virtual_no, $card_no, $physics_no) {
+        $where = [
+            'card_no'       => $card_no,
+            'physics_no'    => $physics_no,
+            '_logic'        => 'OR'
+        ];
+
+        $find = $this->table(self::ANNUAL_CARD_TABLE)->where($where)->find();
+
+        if ($find) {
+            return false;
+        }
+
+        $update = [
+            'card_no'       => $card_no,
+            'physics_no'    => $physics_no
+        ];
+
+        $where = [
+            'sid'           => $sid,
+            'virtual_no'    => $virtual_no,
+            'card_no'       => ''
+        ];
+
+        return $this->table(self::ANNUAL_CARD_TABLE)->where($where)->save($update);
+    }
+
+    /**
+     * 获取年卡库存
+     * @param  [type] $sid  [description]
+     * @param  string $type 虚拟卡 OR 物理卡
+     * @return [type]       [description]
+     */
+    public function getAnnualCardStorage($sid, $type = 'virtual') {
+        if ($type == 'virtual') {
+            $where = [
+                'sid'       => $sid,
+                'card_no'   => '',
+                'status'    => 0
+            ];
+        } else {
+            $where = [
+                'sid'       => $sid,
+                'card'      => array('neq', ''),
+                'status'    => 0
+            ];
+        }
+
+        return $this->table(self::ANNUAL_CARD_TABLE)->where($where)->count();
+    }
     
 }
