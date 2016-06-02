93a1c72e7d1b579aafced4d29ba635daa1f61416
chenguangpeng <1031289030@qq.com>
2016-06-02 19:02:32 +0800

    excel导出

--- Controller/Order/OrderQuery.class.php
+++ Controller/Order/OrderQuery.class.php
@@ -382,6 +382,11 @@ class OrderQuery extends Controller
      */
     protected function format_order_data( $excel = false )
     {
+        if ($excel===true) {
+            self::download_send_headers("data_export_" . date("Y-m-d") . ".csv");
+            self::array2csv($this->orders);
+            exit;
+        }
         foreach ($this->orders as $_ordernum=>$orders) {
             //main
             $this->output[$_ordernum] = [];
@@ -421,5 +426,47 @@ class OrderQuery extends Controller
             //price handler end
         }//End foreach
         return true;
+    }
+    static function download_send_headers($filename) {
+        // disable caching
+        $now = gmdate("D, d M Y H:i:s");
+        header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+        header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
+        header("Last-Modified: {$now} GMT");
+
+        // force download
+        header("Content-Type: application/force-download");
+        header("Content-Type: application/octet-stream");
+        header("Content-Type: application/download");
+
+        // disposition / encoding on response body
+        header("Content-Disposition: attachment;filename={$filename}");
+        header("Content-Transfer-Encoding: binary");
+    }
+    static function array2csv(array &$array)
+    {
+        //print_r($array);exit;
+        if (count($array) == 0) {
+            return null;
+        }
+        ob_start();
+        $df = fopen("php://output", 'w');
+        $excel = [];
+        foreach ($array as $item) {
+            $excel[] = $item['main'];
+            if (isset($item['links'])) {
+                foreach ($item['links'] as $link) {
+                    $excel[] = $link;
+                }
+            }
+        }
+        //print_r($excel);exit;
+        fputcsv($df, array_keys(reset($excel)));
+        foreach ($excel as $row) {
+            fputcsv($df, $row);
+        }
+        fclose($df);
+        echo ob_get_clean();
+        //return ob_get_clean();
     }
 }
