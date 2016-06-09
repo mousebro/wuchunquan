15132e4432aea2f44897f55e95916d65ede88f02
zhang_dewei <zhang_dewei@sina.cn>
2016-06-09 18:07:35 +0800

    添加自动清分控制器和自动脚本

--- Controller/Finance/SettleBlance.class.php
+++ Controller/Finance/SettleBlance.class.php
@@ -17,6 +17,56 @@ use Library\Controller;
 class SettleBlance extends Controller {
     
     public function __construct() }{
+        
+    }
 
+    /**
+     * 添加配置记录 
+     * @author dwer
+     * @date   2016-06-09
+     *
+     */
+    public function add() {
+        
+    }
+
+    /**
+     * 修改配置记录 
+     * @author dwer
+     * @date   2016-06-09
+     *
+     */
+    public function udpate() {
+        
+    }
+
+    /**
+     * 获取某条配置的具体信息
+     * @author dwer
+     * @date   2016-06-09
+     *
+     */
+    public function getSettingInfo() {
+
+    }
+
+    /**
+     * 获取某条配置的列表
+     * @author dwer
+     * @date   2016-06-09
+     *
+     */
+    public function getSettingList() {
+
+    }
+
+    /**
+     * 设置配置的状态
+     * @author dwer
+     * @date   2016-06-09
+     *
+     */
+    public function setStatus() {
+        
     }
 }
--- Controller/cli/SettleBlance.class.php
+++ Controller/cli/SettleBlance.class.php
@@ -1,4 +1,4 @@
-<?php if(!defined('PFT_CLI')) {}
+<?php if(!defined('PFT_CLI')) {exit('Access Deny');}
 /**
  * 用户余额清分后台配置控制器
  * 清分模型：1=日结，2=周结，3=月结
@@ -18,5 +18,40 @@ class SettleBlance extends Controller {
     public function __construct() }{
         //做下运行模式的判断
 
+    }
+
+    /**
+     * 按设定的规则生成清分记录
+     * 可以一个小时执行一次
+     * 
+     * @author dwer
+     * @date   2016-06-09
+     *
+     * @return 
+     */
+    public function generateTransRecord() {
+
+    }
+
+    /**
+     * 运行清算任务
+     * @author dwer
+     * @date   2016-06-09
+     *
+     * @return 
+     */
+    public function runSettleTask() {
+        
+    }
+
+    /**
+     * 运行打款任务
+     * @author dwer
+     * @date   2016-06-09
+     *
+     * @return
+     */
+    public function runTransTask() {
+        
     }
 }
