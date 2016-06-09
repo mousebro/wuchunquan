a5527559491301d3f1a2c7f4aaa7fabc0fc34980
zhang_dewei <zhang_dewei@sina.cn>
2016-06-09 17:51:56 +0800

    大致完成自动转账的模型部分

--- Model/Finance/SettleBlance.class.php
+++ Model/Finance/SettleBlance.class.php
@@ -26,17 +26,34 @@ class SettleBlance extends Model{
      * @param $fid 用户ID
      * @param $mode 自动清分模式，1=日结，2=周结，3=月结
      * @param $freezeType 资金冻结类型，1=冻结未使用的总额，2=按比例冻结
-     * @param $closeTime 结算时间，日结（几点），月结（几号），周结（周几）
-     * @param $transferTime 转账日期，日结（几点），月结（几号），周结（周几）
+     * @param $closeDate 结算日期，日结（几点），月结（几号），周结（周1-周7）
+     * @param $closeTime 结算时间，具体几点
+     * @param $transferDate 转账日期，日结（几点），月结（几号），周结（周几）
+     * @param $transferTime 转账时间，具体几点
      * @param $updateUid 配置修改用户ID
-     * @param $accountInfo '账号的json信息串 {"bank_name":"","bank_ins_code":"","bank_account":"","acc_type":""}',
+     * @param $accountInfo '账号的数组 {"bank_name":"","bank_ins_code":"","bank_account":"","acc_type":""}',
      * @param $freezeData 资金冻结详情，比例或是具体的金额 - {"type":"1/2","value":"30"}
      *
      * @return bool
      */
-    public function addSetting($fid, $mode, $freezeType, $closeTime, $transferTime, $updateUid, $accountInfo, $freezeData = false) {
+    public function addSetting($fid, $mode, $freezeType, $closeDate, $closeTime, $transferDate, $transferTime, $updateUid, $accountInfo,
+            $freezeData = false) {
 
-        return true;
+        if(!$fid) {
+            return false;
+        }
+
+        //参数统一校验，格式化
+        $data = $this->_formatParam($mode, $freezeType, $closeDate, $closeTime, $transferDate, $transferTime, $updateUid, $accountInfo,
+            $freezeData);
+
+        if($data) {
+            return false;
+        }
+
+        $res = $this->table($this->_settingTable)->add($data);
+
+        return $res === false ? false : true;
     }
 
     /**
@@ -47,15 +64,33 @@ class SettleBlance extends Model{
      * @param $id 记录ID
      * @param $mode 自动清分模式，1=日结，2=周结，3=月结
      * @param $freezeType 资金冻结类型，1=冻结未使用的总额，2=按比例冻结
-     * @param $closeTime 结算时间，日结（几点），月结（几号），周结（周几）
-     * @param $transferTime 转账日期，日结（几点），月结（几号），周结（周几）
+     * @param $closeDate 结算日期，日结（几点），月结（几号），周结（周1-周7）
+     * @param $closeTime 结算时间，具体几点
+     * @param $transferDate 转账日期，日结（几点），月结（几号），周结（周几）
+     * @param $transferTime 转账时间，具体几点
      * @param $updateUid 配置修改用户ID
+     * @param $accountInfo '账号的数组 {"bank_name":"","bank_ins_code":"","bank_account":"","acc_type":""}',
      * @param $freezeData 资金冻结详情，比例或是具体的金额 - {"type":"1/2","value":"30"}
      *
      * @return bool
      */
-    public function updateSetting($id, $mode, $freezeType, $closeTime, $transferTime, $updateUid, $freezeData) {
+    public function updateSetting($id, $$mode, $freezeType, $closeDate, $closeTime, $transferDate, $transferTime, $updateUid, $accountInfo,
+            $freezeData = false) {
+        if(!$id) {
+            return false;
+        }
 
+        //参数统一校验，格式化
+        $data = $this->_formatParam($mode, $freezeType, $closeDate, $closeTime, $transferDate, $transferTime, $updateUid, $accountInfo,
+            $freezeData);
+
+        if($data) {
+            return false;
+        }
+
+        $res = $this->table($this->_settingTable)->where(['id' => $id])->save($data);
+
+        return $res === false ? false : true;
     }
 
     /**
@@ -68,7 +103,16 @@ class SettleBlance extends Model{
      * @return
      */
     public function updateCircle($id, $circleMark) {
+        $circleMark = intal($circleMark);
+        if(!$id || !$circleMark) {
+            return false;
+        }
 
+        $where = ['id' => $id];
+        $data = ['circel_mark' => $circleMark];
+
+        $res = $this->table($this->_settingTable)->where($where)->save($data);
+        return $res === false ? false : true;
     }
 
     /**
@@ -77,15 +121,28 @@ class SettleBlance extends Model{
      * @date   2016-06-08
      *
      * @param  $id 记录ID
+     * @param  $updateUid 更新用户UID
      * @param  $status 状态 off=无效，on=有效
      * @return
      */
-    public function settingStatus($id, $status = 'off') {
+    public function settingStatus($id, $updateUid, $status = 'off') {
+        if(!$id || !$updateUid || !in_array($status, ['on', 'off'])) {
+            return false;
+        }
 
+        $where = ['id' => $id];
+        $data = [
+            'status'      => ($status == 'on' ? 1 : 0),
+            'update_uid'  => $updateUid,
+            'update_time' => time()
+        ];
+
+        $res = $this->table($this->_settingTable)->where($where)->save($data);
+        return $res === false ? false : true;
     }
 
     /**
-     *  
+     * 获取清分配置列表
      * @author dwer
      * @date   2016-06-08
      *
@@ -93,12 +150,29 @@ class SettleBlance extends Model{
      * @param  $size 条目数
      * @param  $fid 用户ID 如果有搜索用户的话，传ID
      * @param  $mode 自动清分模式，1=日结，2=周结，3=月结
-     * @param  $closeTime 结算时间，日结（几点），月结（几号），周结（周几）
      * @param  $circleMark 上次更新的周期标识，日（20160612）-月（20160104）-周（20160238）
      * @return
      */
-    public function getSettingList($page, $size, $fid = false, $mode = false, $closeTime = false, $circleMark = false) {
+    public function getSettingList($page, $size, $fid = false, $mode = false, $circleMark = false) {
+        $page = intval($page);
+        $size = intval($size);
 
+        $where = [];
+        if($fid) {
+            $where['fid'] => intval($fid);
+        }
+
+        if($mode) {
+            $where['mode'] => intval($mode);
+        }
+
+        if($circleMark) {
+            $where['circle_mark'] => ['lt', intval($mode)];
+        }
+
+        $res = $this->table($this->_settingTable)->where($where)->page($page . ',' . $size)->select();
+
+        return $res === false ? [] : $res;
     }
 
     /**
@@ -113,7 +187,25 @@ class SettleBlance extends Model{
      * @return
      */
     public function createAutoRecord($fid, $settleTime, $transferTime, $cycleMark) {
+        if(!$fid || !$settleTime || !$transferTime || !$cycleMark) {
+            return falsel;
+        }
+        
+        $settleTime   = intval($settleTime);
+        $transferTime = intval($transferTime);
+        $cycleMark    = intval($cycleMark);
 
+        $data = [
+            'fid'           => $fid,
+            'settle_time'   => $settleTime,
+            'transfer_time' => $transferTime,
+            'circle_mark'   => $cycleMark,
+            'update_time'   => time()
+        ];
+
+        $res = $this->table($this->_recordTable)->add($data);
+
+        return $res === false ? false : true;
     }
 
     /**
@@ -122,13 +214,39 @@ class SettleBlance extends Model{
      * @date   2016-06-08
      *
      * @param  $id 记录ID
-     * @param  $freezeMoney 冻结的金额
-     * @param  $transferMoney 具体需要转账的金额
+     * @param  $freezeMoney 冻结的金额 - 分
+     * @param  $transferMoney 具体需要转账的金额 - 分
      * @param  $remark
      * @return
      */
-    public function updateSettleInfo($id, $freezeMoney, $transferMoney, $remark) {
+    public function updateSettleInfo($id, $freezeMoney, $transferMoney, $remark = false) {
+        $id = intval($id);
+        $freezeMoney = intval($freezeMoney);
+        $transferMoney = intval($transferMoney);
 
+        if($remark) {
+            $remark = strval($remark);
+        }
+
+        if(!$id || $transferMoney <= 0) {
+            return false;
+        }
+
+        $data = [
+            'fid'            => $fid,
+            'freeze_money'   => $freezeMoney,
+            'transfer_money' => $transferMoney,
+            'is_settle'      => 0,
+            'is_transfer'    => 0,
+            'update_time'    => time()
+        ];
+        
+        if($remark) {
+            $data['remark'] = $remark;
+        }
+
+        $res = $this->table($this->_recordTable)->where(['id' => $id])->save($data);
+        return $res === false ? false : true;
     }
 
     /**
@@ -141,7 +259,17 @@ class SettleBlance extends Model{
      * @return
      */
     public function updateTransferInfo($id, $status) {
+        if(!$id || !in_array($status, [0, 1, 2])) {
+            return false;
+        }
 
+        $data = [
+            'status'      => $status,
+            'update_time' => time()
+        ];
+
+        $res = $this->table($this->_recordTable)->where(['id' => $id])->save($data);
+        return $res === false ? false : true;
     }
 
     /**
@@ -154,7 +282,19 @@ class SettleBlance extends Model{
      * @return
      */
     public function getSettleList($page, $size) {
+        $page = intval($page);
+        $size = intval($size);
 
+        $where = [
+            'status'      => 0,
+            'is_settle'   => 0,
+            'is_transfer' => 0,
+            'settle_time' => ['gt', time()]
+        ];
+
+         $res = $this->table($this->_recordTable)->where($where)->page($page . ',' . $size)->select();
+
+         return $res === false ? [] : $res;
     }
 
     /**
@@ -167,9 +307,76 @@ class SettleBlance extends Model{
      * @return
      */
     public function getTransferList($page, $size) {
+        $page = intval($page);
+        $size = intval($size);
 
+        $where = [
+            'status'        => 0,
+            'is_settle'     => 1,
+            'is_transfer'   => 0,
+            'transfer_time' => ['gt', time()]
+        ];
+
+         $res = $this->table($this->_recordTable)->where($where)->page($page . ',' . $size)->select();
+
+         return $res === false ? [] : $res;
     }
 
+    /**
+     * 格式化参数
+     * @author dwer
+     * @date   2016-06-09
+     *
+     * @param $mode 自动清分模式，1=日结，2=周结，3=月结
+     * @param $freezeType 资金冻结类型，1=冻结未使用的总额，2=按比例冻结
+     * @param $closeDate 结算日期，日结（几点），月结（几号），周结（周1-周7）
+     * @param $closeTime 结算时间，具体几点
+     * @param $transferDate 转账日期，日结（几点），月结（几号），周结（周几）
+     * @param $transferTime 转账时间，具体几点
+     * @param $updateUid 配置修改用户ID
+     * @param $accountInfo '账号的数组 {"bank_name":"","bank_ins_code":"","bank_account":"","acc_type":""}',
+     * @param $freezeData 资金冻结详情，比例或是具体的金额 - {"type":"1/2","value":"30"}
+     * @return array / bool
+     */
+    private function _formatParam($mode, $freezeType, $closeDate, $closeTime, $transferDate, $transferTime, $updateUid, $accountInfo,
+            $freezeData = false) {
+
+        if(!in_array($mode, [1, 2, 3]) || !$updateUid || !$accountInfo || !is_array($accountInfo)) {
+            return false;
+        }
+
+        $closeDate    = intval($closeDate);
+        $closeTime    = intval($closeTime);
+        $transferDate = intval($transferDate);
+        $transferTime = intval($transferTime);
+        $accountInfo  = json_encode($accountInfo);
+
+        if($freezeType == 2) {
+            if(!$freezeData || !is_array($freezeData)) {
+                return false;
+            }
+
+            $freezeData = json_encode($freezeData);
+        }
+
+        $data = [
+            'mode'          => $mode,
+            'freeze_type'   => $freezeType,
+            'close_date'    => $closeDate,
+            'close_time'    => $closeTime,
+            'transfer_date' => $transferDate,
+            'transfer_time' => $transferTime,
+            'account_info'  => $accountInfo,
+            'update_uid'    => $updateUid,
+            'update_time'   => time()
+        ];
+
+        if($freezeType == 2) {
+            $data['freeze_data'] = $freezeData;
+        }
+
+        return $data;
+    }
 
 
 }
