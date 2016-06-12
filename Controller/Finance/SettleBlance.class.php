4d9e3fe3486ebcde23a6ce7922fd8cf928daaa33
dewei <zhang_dewei@qq.com>
2016-06-12 18:46:51 +0800

    后台配置接口

--- Controller/Finance/SettleBlance.class.php
+++ Controller/Finance/SettleBlance.class.php
@@ -13,11 +13,35 @@
 namespace Controller\Finance;
 
 use Library\Controller;
+use Model\Member\Member;
 
 class SettleBlance extends Controller {
-    
-    public function __construct() }{
+    private $_memberId = null;
+
+    private $_modeArr = [
+        1, // 日结
+        2, // 周结
+        3, // 月结
+    ];
+
+    private $_freezeArr = [
+        1, // 冻结未使用的总额
+        2, // 按比例冻结
+    ];
+
+    private $_accountArr = [
+        1, //银行账户1
+        2, //银行账号2
+    ]; 
+
+    public function __construct() {
+        //只有管理员才能进行操作
+        $tmpId = 1;//$this->isLogin('ajax');
+
+        //角色判斷
         
+
+        $this->_memberId = $tmpId;
     }
 
     /**
@@ -26,8 +50,55 @@ class SettleBlance extends Controller {
      * @date   2016-06-09
      *
      */
-    public function add() {
+    public function add($updateId) {
+        //参数过滤
+        $fid          = intval(I('post.fid'));
+        $mode         = intval(I('post.mode'));
+        $freezeType   = intval(I('post.freeze_type'));
+        $closeDate    = intval(I('post.close_date'));
+        $closeTime    = intval(I('post.close_ime'));
+        $transferDate = intval(I('post.transfer_date'));
+        $transferTime = intval(I('post.transfer_time'));
+        $accountNo    = intval(I('post.account_no'));
+
+        //参数合法性
+        if(!$fid || !in_array($mode, $this->_modeArr) || !in_array($freezeType, $this->_freezeArr) || !in_array($accountNo, $this->_accountArr)) {
+            $this->apiReturn(401, [], '');
+        }
         
+        //时间验证
+        if($mode == 1) {
+
+        } else if($mode == 2) {
+
+        } else {
+
+        }
+
+        //获取用户信息
+        $memberModel = $this->model('Member/Member');
+        $memberInfo = $memberModel->getMemberInfo($uid);
+
+        //银行账户信息验证
+
+
+        //冻结资金配置
+        if($freezeType == 2) {
+
+        }
+
+        $settleBlanceModel = $this->model('Finance/SettleBlance');
+        if($updateUid) {
+            $res = $settleBlanceModel->updateSetting($updateUid, $mode, $freezeType, $closeDate, $closeTime, $transferDate, $transferTime, $updateUid, $accountInfo,$freezeData);
+        } else {
+            $res = $settleBlanceModel->addSetting($fid, $mode, $freezeType, $closeDate, $closeTime, $transferDate, $transferTime, $updateUid, $accountInfo,$freezeData);
+        }
+
+        if($res) {
+            $this->apiReturn(200, [], '');
+        } else {
+            $this->apiReturn(500, [], '');
+        }
     }
 
     /**
@@ -37,7 +108,13 @@ class SettleBlance extends Controller {
      *
      */
     public function udpate() {
-        
+        $updateId = intval(I('post.id'));
+        //参数合法性
+        if(!$updateId) {
+            $this->apiReturn(401, [], '');
+        }
+
+        $this->add($updateId);
     }
 
     /**
@@ -47,6 +124,12 @@ class SettleBlance extends Controller {
      *
      */
     public function getSettingInfo() {
+        $updateId = intval(I('post.id'));
+        //参数合法性
+        if(!$updateId) {
+            $this->apiReturn(401, [], '');
+        }
+
 
     }
 
@@ -68,5 +151,95 @@ class SettleBlance extends Controller {
      */
     public function setStatus() {
         
+    }
+
+    /**
+     * 获取常用的银行账户
+     * @author dwer
+     * @date   2016-06-12
+     *
+     * @return
+     */ 
+    public function getAccounts() {
+        $uid = intval(I('post.uid'));
+        if(!$uid) {
+            $this->apiReturn(401, [], '');
+        }
+
+        $memberModel = $this->model('Member/Member');
+        $memberInfo = $memberModel->getMemberInfo($uid);
+        if(!$memberInfo) {
+            $this->apiReturn(402, [], '');
+        }
+
+        $account1 = $memberInfo['bank_account1'];
+        $account2 = $memberInfo['bank_account2'];
+
+        $accountArr1 = [];
+        $accountArr2 = [];
+
+        $res = [
+            'account1' => [
+                'status' => 0,
+                'msg'    => '银行账户信息不存在'
+            ],
+            'account2' => [
+                'status' => 0,
+                'msg'    => '银行账户信息不存在'
+            ],
+        ];
+
+        //获取具体账号信息
+        if($account1) {
+            $tmp = explode('|', $account1);
+
+            if(count($tmp) < 5) {
+                $res['account1']['status'] = 0;
+                $res['account1']['msg']    = '银行账户信息缺失，请重新编辑';
+            } else {
+                $insCode = $tmp['1'];
+                if(!preg_match('/[0-9]{12}/', $insCode)) {
+                    $res['account1']['status'] = 0;
+                    $res['account1']['msg']    = '现代化支付系统行号錯誤，请重新编辑';
+                } else {
+                    $res['account1']['status']        = 1;
+                    $res['account1']['msg']           = '';
+                    $res['account1']['bank_name']     = $tmp[0];
+                    $res['account1']['bank_name']     = $tmp[0];
+                    $res['account1']['bank_ins_code'] = $tmp[1];
+                    $res['account1']['bank_account']  = $tmp[2];
+                    $res['account1']['account_name']  = $tmp[3];
+                    $res['account1']['accType']       = $tmp[4];
+                }
+            }
+        }
+
+        //获取具体账号信息 
+        if($account2) {
+            $tmp = explode('|', $account2);
+
+            if(count($tmp) < 5) {
+                $res['account2']['status'] = 0;
+                $res['account2']['msg']    = '银行账户信息缺失，请重新编辑';
+            } else {
+                $insCode = $tmp['1'];
+                if(!preg_match('/[0-9]{12}/', $insCode)) {
+                    $res['account2']['status'] = 0;
+                    $res['account2']['msg']    = '现代化支付系统行号錯誤，请重新编辑';
+                } else {
+                    $res['account2']['status']        = 1;
+                    $res['account2']['msg']           = '';
+                    $res['account2']['bank_name']     = $tmp[0];
+                    $res['account2']['bank_name']     = $tmp[0];
+                    $res['account2']['bank_ins_code'] = $tmp[1];
+                    $res['account2']['bank_account']  = $tmp[2];
+                    $res['account2']['account_name']  = $tmp[3];
+                    $res['account2']['accType']       = $tmp[4];
+                }
+            }
+        }
+
+        //返回账号信息
+        $this->apiReturn(200, $res);
     }
 }
