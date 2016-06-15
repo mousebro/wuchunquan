4e747815b87fda3edcb972275192a6bb4ba1490c
chenguangpeng <1031289030@qq.com>
2016-06-15 18:58:54 +0800

    order model

--- Model/Order/OrderSubmit.class.php
+++ Model/Order/OrderSubmit.class.php
@@ -31,4 +31,45 @@ class OrderSubmit extends Model
             ->limit(1)
             ->getField('id');
     }
+    /**
+     * 订单编号生成规则，n(n>=1)个订单表对应一个支付表，
+     * 生成订单编号(年取1位 + $pay_id取13位 + 第N个子订单取2位)
+     * 1000个会员同一微秒提订单，重复机率为1/100
+     * @param int $pay_id 支付表自增ID
+     * @return string
+     * copy from shopnc
+     */
+    public function makeOrderSn($pay_id) {
+        //记录生成子订单的个数，如果生成多个子订单，该值会累加
+        static $num;
+        if (empty($num)) {
+            $num = 1;
+        } else {
+            $num ++;
+        }
+        return (date('y',time()) % 9+1) . sprintf('%013d', $pay_id) . sprintf('%02d', $num);
+    }
+    public  function getOrderSn() {
+        $id = $this->table('pft_order_key')->add(['id'=>'null']);
+        return $this->makeOrderSn($id);
+    }
+
+    public function addOrder($params)
+    {
+        return $this->table(self::__TBL_ORDER__)->data($params)->add();
+    }
+
+    public function addOrderApplyInfo($params)
+    {
+        return $this->table('uu_order_apply_info')->data($params)->add();
+    }
+
+    public function addOrderAddon($params)
+    {
+        return $this->table('uu_order_addon')->data($params)->add();
+    }
+    public function addOrderDetail($params)
+    {
+        return $this->table('uu_order_fx_detail')->data($params)->add();
+    }
 }
