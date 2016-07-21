2d4b76fcc78748b8b02cc5ba3913d1a0ca1493a2
liubb <liubb@12301.com>
2016-07-21 10:51:00 +0800

    异常订单

--- Model/Order/orderAbnormal.class.php
+++ Model/Order/orderAbnormal.class.php
@@ -10,9 +10,9 @@ use Library\Model;
 class orderAbnormal extends Model {
 
     const UU_QUNAR_USE = 'uu_qunar_use';
-    const ALL_API_ORDER = 'all_api_order';
     const UU_JQ_TICKET = 'uu_jq_ticket';
     const UU_LAND = 'uu_land';
+    const PFTCONSYS = 'pft_con_sys';
     //对接系统标识码 0去哪儿 20美团直连 13百度直达 21美团 22糯米 23美团V2
     const GROUPON_IDENT = array(0,20,13,21,22,23);
     //异常订单状态码
@@ -31,120 +31,6 @@ class orderAbnormal extends Model {
         );
         $data = $this->table(self::UU_QUNAR_USE)->field('tid', 'coop_id')->where($params)->select();
         return $data;
-    }
-
-    /**
-     * 获取第三方异常订单（管理员）
-     * handleStatus in (1,2)  1失败 2超时   状态
-     * coopB not in (0,20,13,21,22,23)    对接系统标识码 0去哪儿 20美团直连 13百度直达 21美团 22糯米
-     * @param $bTime 开始时间 eq:2016-07-14 00:00:00
-     * @param $eTime 结束时间 eq:2016-07-14 00:00:00
-     * @param $start 从第几条数据开始 默认0
-     * @param $size  一次性获取几条数据 默认15
-     * @return 
-     */
-    public function getOrderForManager($bTime, $eTime, $start = 0, $size = 15) {
-        if (empty($bTime) || empty($eTime) || !is_numeric($start) || !is_numeric($size)) {
-            return false;
-        }
-        $params = array(
-            'A.cTime' => array(
-                array('gt', $bTime),
-                array('lt', $eTime),
-            ),
-            'A.handleStatus' => array('in', self::ERRORSTATUS),
-            'A.coopB' => array('not in', self::GROUPON_IDENT),
-        );
-        $table = self::ALL_API_ORDER;
-        $table = $table.' A';
-        $data = $this->table($table)->field('A.*, C.name')
-                                        ->join('left join pft_con_sys C on A.coopB=C.coopB')
-                                        ->where($params)
-                                        ->order('id desc')
-                                        ->limit($start, $size)
-                                        ->select();
-        $count = $this->table($table)->field('A.*, C.name')
-                                        ->join('left join pft_con_sys C on A.coopB=C.coopB')
-                                        ->where($params)
-                                        ->count();
-        if (!is_array($data)) {
-            return false;
-        }
-        if (empty($data)) {
-            return array(
-                'count' => $count,
-                'data' => $data,
-            );
-        }
-        $ticketIdArr = array_column($data, 'bCode');
-        $ticketName = $this->getTicketName($ticketIdArr);
-        return array(
-            'count' => $count,
-            'data' => $data,
-            'ticket' => $ticketName,
-        );
-    }
-
-    /**
-     * 获取第三方异常订单（供销商）
-     * handleStatus in (1,2)  1失败 2超时   状态
-     * coopB not in (0,20,13,21,22,23)    对接系统标识码 0去哪儿 20美团直连 13百度直达 21美团 22糯米
-     * @param $fid   分销商ID
-     * @param $bTime 开始时间 eq:2016-07-14 00:00:00
-     * @param $eTime 结束时间 eq:2016-07-14 00:00:00
-     * @param $start 从第几条数据开始 默认0
-     * @param $size  一次性获取几条数据 默认15
-     * @return 
-     */
-    public function getOrderNotForManager($fid, $bTime, $eTime, $start = 0, $size = 15) {
-        if (!is_numeric($fid) || empty($bTime) || empty($eTime) || !is_numeric($start) || !is_numeric($size)) {
-            return false;
-        }
-        //获取自己供应的产品
-        $applyDidArr = $this->getTickIdByApplyId($fid);
-        if (!is_array($applyDidArr)) {
-            return false;
-        }
-        $condition['A.fid'] = $fid;
-        $condition['A.bCode'] = array('in', $applyDidArr);
-        $condition['_logic'] = 'OR';
-        $params = array(
-            '_complex' => $condition,
-            'A.cTime' => array(
-                array('gt', $bTime),
-                array('lt', $eTime),
-            ),
-            'A.handleStatus' => array('in', self::ERRORSTATUS),
-            'A.coopB' => array('not in', self::GROUPON_IDENT),
-        );
-        $table = self::ALL_API_ORDER;
-        $table = $table.' A';
-        $data = $this->table($table)->field('A.*, C.name')
-                                        ->join('left join pft_con_sys C on A.coopB=C.coopB')
-                                        ->where($params)
-                                        ->order('id desc')
-                                        ->limit($start, $size)
-                                        ->select();
-        $count = $this->table($table)->field('A.*, C.name')
-                                        ->join('left join pft_con_sys C on A.coopB=C.coopB')
-                                        ->where($params)
-                                        ->count();
-        if (!is_array($data)) {
-            return false;
-        }
-        if (empty($data)) {
-            return array(
-                'count' => $count,
-                'data' => $data,
-            );
-        }
-        $ticketIdArr = array_column($data, 'bCode');
-        $ticketName = $this->getTicketName($ticketIdArr);
-        return array(
-            'count' => $count,
-            'data' => $data,
-            'ticket' => $ticketName,
-        );
     }
 
     /**
@@ -274,5 +160,25 @@ class orderAbnormal extends Model {
             'data' => $data,
             'ticket' => $ticketName,
         );
+    }
+
+    /**
+     * 通过coopB获取name（pft_con_sys表）
+     * @param int $coopB
+     * @return array
+     * @author liubb
+     */
+    public function getInfoByCoopB($coopB) {
+        if (!isset($coopB) || !is_numeric($coopB)) {
+            return false;
+        }
+        $params = array(
+            'coopB' => $coopB,
+        );
+        $res = $this->table(self::PFTCONSYS)->field('name')->where($params)->getField('name');
+        if (!$res) {
+            return false;
+        }
+        return $res;
     }
 }
