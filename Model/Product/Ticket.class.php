361e085824d69bf9e8e1e8059b65e7caecbfac2a
chenguangpeng <1031289030@qq.com>
2016-07-22 12:03:23 +0800

    门票上下架添加pid条件

--- Model/Product/Ticket.class.php
+++ Model/Product/Ticket.class.php
@@ -744,13 +744,16 @@ class Ticket extends Model {
         return $res;
     }
 
-    public function SetTicketStatus($tid, $status, $memberId)
+    public function SetTicketStatus($tid, $status, $memberId, $pid=0)
     {
+        $map = [];
+        if ($pid>0) $map['p.id']=$pid;
+        else $map['t.id'] = $tid;
         $info = $this->QueryTicketInfo(
-            ['t.id'=>$tid],
+            $map,
             'p.apply_limit,p.apply_did,p.id as pid,p.p_name',
             'inner join uu_products p on t.pid=p.id'
-            );
+        );
         if(!$info) return ['code'=>0, "msg"=>"门票不存在"];
         $info = array_shift($info);
         if($memberId!=$info['apply_did']) return ['code'=>0, "msg"=>"非自身供应产品"];
