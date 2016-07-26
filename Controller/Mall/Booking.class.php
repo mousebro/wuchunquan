b4068943dc2f987f65f444ffd2393250a353e9c1
wengbin <379591850@qq.com>
2016-07-26 18:51:23 +0800

    update

--- Controller/Mall/Booking.class.php
+++ Controller/Mall/Booking.class.php
@@ -28,38 +28,80 @@ class Booking extends Controller {
             }
         }
 
-        $TicketModel = new Ticket();
-        $MemberModel = new Member();
+        $ptype = $this->model('Product\Ticket')->getProductType($pids[0]);
 
-        $memberid = $aid = 0;
+        $date = I('date') ?: date('Y-m-d');
 
-        $shop_account = (int)explode('.', $_SERVER['HTTP_HOST'])[0];
-        $shop_own = $MemberModel->getMemberInfo($shop_account, 'account');
-        $shop_id = $shop_own['id'];
-
-        $storages = [];
-        foreach ($pids as $pid) {
-            if (!$TicketModel->isSelfApplyProduct($shop_id, $pid)) {
-                $tinfo = $TicketModel->getTicketInfoByPid($pid);
-                $memberid = $shop_id;
-                $aid = $tinfo['apply_did'];
-            } else {
-                //分销商库存
-                if (isset($_SESSION['dtype']) && in_array($_SESSION['dtype'], [0,1])) {
-                    $memberid = $_SESSION['memberID'];
-                    $aid = I('aid', '', 'intval');
-
-                } else {
-                    $memberid = $aid = I('aid', '', 'intval');
-                }
-            }
-            $storage = $TicketModel->getMuchStorage([$pid], I('date') ?: date('Y-m-d'), $memberid, $aid);
-            $storages[$pid] = (int)$storage[$pid];
-
+        if ($ptype == 'A') {
+            //普通产品获取库存
+            $storages = $this->_getStorageForCommon($pids, $date);
+        } elseif ($ptype == 'H') {
+            //获取演出类库存
+            $storages = $this->_getStorageForShow($pid[0], 6970);
         }
 
         $this->apiReturn(200, $storages, '');
 
+    }
+
+    /**
+     * 获取普通产品的库存
+     * @param  array  $pids 产品id数组，[3026, 3027]
+     * @param  string $date 日期
+     * @return [type]       [description]
+     */
+    private function _getStorageForCommon($pids, $date) {
+
+        $TicketModel = $this->model('Product\Ticket');
+
+        $memberid = $aid = 0;
+        $shop_id = $this->_getSupplyId();
+
+        $storages = [];
+        foreach ($pids as $pid) {
+            if (!$TicketModel->isSelfApplyProduct($shop_id, $pid)) {
+
+                $tinfo = $TicketModel->getTicketInfoByPid($pid);
+                $memberid = $shop_id;
+                $aid = $tinfo['apply_did'];
+
+            } else {
+                //分销商库存
+                if (isset($_SESSION['dtype']) && in_array($_SESSION['dtype'], [0,1])) {
+
+                    $memberid = $_SESSION['memberID'];
+                    $aid = I('aid', '', 'intval');
+
+                } else {
+
+                    $memberid = $aid = I('aid', '', 'intval');
+
+                }
+            }
+
+            $storage = $TicketModel->getMuchStorage([$pid], $date, $memberid, $aid);
+            $storages[$pid] = (int)$storage[$pid];
+
+        }
+        return $storages;
+    }
+
+    /**
+     * 根据域名解析微商城所属供应商id
+     * @return [type] [description]
+     */
+    private function _getSupplyId() {
+
+        $shop_account = (int)explode('.', $_SERVER['HTTP_HOST'])[0];
+        $shop_own = $this->model('Member/Member')->getMemberInfo($shop_account, 'account');
+
+        return $shop_own['id'];
+
+    }
+
+
+    private function _getStorageForShow($pid, $date) {
+        //pass
     }
 
     /**
@@ -81,8 +123,10 @@ class Booking extends Controller {
 
         $rounds = $this->_getRoundsInfo($productInfo['venus_id'], $date);
 
+        $rounds = $this->_fillStorage($rounds);
+
         if (count($rounds) < 1) {
-            $this->apiReturn(204, '暂无演出场次信息');
+            $this->apiReturn(204, [], '暂无演出场次信息');
         }
 
         $this->apiReturn(200, $rounds);
@@ -106,7 +150,7 @@ class Booking extends Controller {
             $GLOBALS['le'] = $le;
         }
 
-        $p = new \ProductInfo($s,$pid,$aid);
+        $p = new \ProductInfo($s, $pid, $aid);
 
         return $p->pInfo();//返回产品信息
     }
@@ -133,8 +177,6 @@ class Booking extends Controller {
 
             if(strtotime($row['use_date'].' '.$row['et'])<$time) continue;
 
-            unset($row['area_storage']);
-
             $return[] = $row;
         }
 
@@ -144,41 +186,41 @@ class Booking extends Controller {
     /**
      * 填充库存数据
      */
-    // private function _fillStorage($rounds) {
+    private function _fillStorage($rounds) {
 
-    //     $storageModel = new \Model\Product\YXStorage();
+        $storageModel = new \Model\Product\YXStorage();
 
-    //     foreach($rounds as $key => $row){
+        foreach($rounds as $key => $row){
 
-    //         foreach($row['area_storage'] as $areaId => $areaStorage){
+            foreach($row['area_storage'] as $areaId => $areaStorage){
                 
-    //             if (isset($_SESSION['memberID'])) {
-    //                 $memberID = $_SESSION['memberID'];
-    //             } else {
-    //                 $memberID = -1;
-    //             }
+                if (isset($_SESSION['memberID'])) {
+                    $memberID = $_SESSION['memberID'];
+                } else {
+                    $memberID = -1;
+                }
                 
-    //             $sellerStorage = $storageModel->getResellerStorage($memberID, $row['id'], $areaId);
+                $sellerStorage = $storageModel->getResellerStorage($memberID, $row['id'], $areaId);
 
-    //             if($sellerStorage === false) {
-    //                 continue;
-    //             }
+                if($sellerStorage === false) {
+                    continue;
+                }
 
-    //             // 如果分销库存的数量超过的情况
-    //             if($sellerStorage > $areaStorage) {
-    //                 $sellerStorage = $areaStorage;
-    //             }
+                // 如果分销库存的数量超过的情况
+                if($sellerStorage > $areaStorage) {
+                    $sellerStorage = $areaStorage;
+                }
 
-    //             $row['area_storage'][$areaId] = $sellerStorage;
-    //         }
+                $row['area_storage'][$areaId] = $sellerStorage;
+            }
 
-    //         $rounds[$key] = $row;
+            $rounds[$key] = $row;
 
-    //     }
+        }
 
-    //     $storageModel->getLeftStorage();
+        return $rounds;
 
-    // }
+    }
 
 
 }
